7866ce7a87339a29539d022ee3bfa5c598ffea2e
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-08-12 14:11:04 +0200

    Add files via upload

--- Ctxs.h
+++ Ctxs.h
@@ -1,13 +1,11 @@
 #ifndef __CTXS_H
 #define __CTXS_H
 
-#include "Headers/common.h"
-#include "Headers/libwps.h"
-#include "Headers/pixie.h"
-#include "Headers/wpa_common.h"
-#include "Headers/wps.h"
-#include "Headers/wps_defs.h"
-
+#include "C/src/utils/common.h"
+#include "C/src/common/wpa_common.h"
+#include "C/src/wps/wps_defs.h"
+#include "C/src/wps/wps.h"
+#include "C/src/libwps/libwps.h"
 
 #endif /* __CTXS_H */
 

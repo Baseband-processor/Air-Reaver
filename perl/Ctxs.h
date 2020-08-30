ad69082114736ba1b940baa2ff711a7debb8e070
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-08-30 20:15:15 +0000

    Add files via upload

--- perl/Ctxs.h
+++ perl/Ctxs.h
@@ -1,7 +1,12 @@
 #ifndef __CTXS_H
 #define __CTXS_H
 
-#include <reaver/modules.h>
+#include <reaver/src/utils/common.h>
+#include <reaver/src/common/wpa_common.h>
+#include <reaver/src/wps/wps_defs.h>
+#include <reaver/src/wps/wps.h>
+#include <reaver/src/libwps/libwps.h>
+
 #include <pcap.h>
 #include <stdio.h>
 #include <string.h>

4e4ed0433ae60070bb49aa7bcc2746c96433bd47
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-08-30 22:30:10 +0200

    Update Ctxs.h

--- perl/Ctxs.h
+++ perl/Ctxs.h
@@ -1,11 +1,11 @@
 #ifndef __CTXS_H
 #define __CTXS_H
 
-#include <reaver/src/utils/common.h>
-#include <reaver/src/common/wpa_common.h>
-#include <reaver/src/wps/wps_defs.h>
-#include <reaver/src/wps/wps.h>
-#include <reaver/src/libwps/libwps.h>
+#include <reaver/utils/common.h>
+#include <reaver/common/wpa_common.h>
+#include <reaver/wps/wps_defs.h>
+#include <reaver/wps/wps.h>
+#include <reaver/libwps/libwps.h>
 
 #include <pcap.h>
 #include <stdio.h>

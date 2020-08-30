b3b02bbbd7506392a62e4e7980c7c55cc610be4d
Edoardo Mantovani <68107554+Baseband-processor@users.noreply.github.com>
2020-08-30 15:06:53 +0200

    create "modules.h" for include everything

--- perl/Ctxs.h
+++ perl/Ctxs.h
@@ -1,11 +1,10 @@
 #ifndef __CTXS_H
 #define __CTXS_H
 
-#include "C/src/utils/common.h"
-#include "C/src/common/wpa_common.h"
-#include "C/src/wps/wps_defs.h"
-#include "C/src/wps/wps.h"
-#include "C/src/libwps/libwps.h"
+#include <reaver/modules.h>
+#include <pcap.h>
+#include <stdio.h>
+#include <string.h>
 
 #endif /* __CTXS_H */
 
